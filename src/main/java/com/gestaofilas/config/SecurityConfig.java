f7d58ab33a510706a4f9c99451eae51f71e0079f
LehNeves <lneves997@gmail.com>
2019-05-20 15:11:55 -0300

    Implementando JWT parte 2

--- src/main/java/com/gestaofilas/config/SecurityConfig.java
+++ src/main/java/com/gestaofilas/config/SecurityConfig.java
@@ -17,7 +17,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.gestaofilas.security.JWTAuthenticationFilter;
+import com.gestaofilas.security.JWTAuthorizationFilter;
 import com.gestaofilas.security.JWTUtil;
+
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	
 	
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
@@ -46,7 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
-		
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
--- src/main/java/com/gestaofilas/services/UserDetailsServiceImpl.java
+++ src/main/java/com/gestaofilas/services/UserDetailsServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.gestaofilas.dao.ClienteDAO;
 import com.gestaofilas.entity.Cliente;
 import com.gestaofilas.security.UserSS;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 	
 	@Autowired
